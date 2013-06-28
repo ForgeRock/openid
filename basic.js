3ced403398913f366b447e9f0c95adf83ed78f30
Mark Craig <mark.craig@gmail.com>
2013-06-28 17:19:36 +0200

    Respond correctly when denied authorization

--- basic.js
+++ basic.js
@@ -58,3 +58,15 @@ function getParamsFromFragment() {
     }
     return params;
 }
+
+function parseQueryString() {
+    var query = {};
+    var args = document.location.search.substring(1).split('&');
+    for (var arg in args)
+    {
+        var m = args[arg].split('=');
+        query[decodeURIComponent(m[0])] = decodeURIComponent(m[1]);
+    }
+
+    return query;
+}
