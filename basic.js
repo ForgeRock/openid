b848c865b2bc477670aaaa93fd34cf3b1af75b1d
Mark Craig <mark.craig@gmail.com>
2013-06-28 17:54:27 +0200

    Handle deny from end user

--- basic.js
+++ basic.js
@@ -65,3 +65,14 @@ function authHeader(user, password) {
     // console.log("hash: " + hash);
     return "Basic " + hash;
 }
+
+function parseQueryString() {
+    var query = {};
+    var args = document.location.search.substring(1).split('&');
+    for (var arg in args)
+    {
+        var m = args[arg].split('=');
+        query[decodeURIComponent(m[0])] = decodeURIComponent(m[1]);
+    }
+    return query;
+}
