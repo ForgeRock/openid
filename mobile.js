813a9c61e35b715ee390ef3683dd15ab7cf51a76
Mark Craig <mark.craig@forgerock.com>
2014-10-17 14:37:52 +0200

    Add GSMA Mobile Connect experiment

---
+++ mobile.js
@@ -0,0 +1,20 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * If applicable, add the following below this MPL 2.0 HEADER, replacing
+ * the fields enclosed by brackets "[]" replaced with your own identifying
+ * information:
+ *     Portions Copyright [yyyy] [name of copyright owner]
+ *
+ *     Copyright 2014 ForgeRock AS
+ *
+ */
+// START CONFIGURATION...
+
+var acr_values = "2";
+var nonce      = "n-0S6_WzA2Mj";
+var login_hint = "msisdn=15554567890";
+
+// ...END CONFIGURATION
